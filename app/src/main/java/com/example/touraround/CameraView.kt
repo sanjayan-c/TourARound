2da8084db1a6026ddb2e945ff5dd518efd270713
sanja <sanjayan201@gmail.com>
2023-09-03 13:47:27 +0530

    Arrow orientation

--- app/src/main/java/com/example/touraround/CameraView.kt
+++ app/src/main/java/com/example/touraround/CameraView.kt
@@ -12,7 +12,12 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -35,7 +40,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class CameraView : AppCompatActivity() {
+class CameraView : AppCompatActivity(), SensorEventListener {
     private lateinit var toggleFlash: ImageButton
     private lateinit var previewView: PreviewView
     private var cameraFacing = CameraSelector.LENS_FACING_BACK
@@ -45,6 +50,12 @@ class CameraView : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val destination = LatLng(6.971339883324587, 79.87446757262208) // Los Angeles
 
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+    private var magnetometer: Sensor? = null
+    private var accelerometerReading = FloatArray(3)
+    private var magnetometerReading = FloatArray(3)
+    private lateinit var arrowImageView: ImageView
 
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
@@ -54,11 +65,13 @@ class CameraView : AppCompatActivity() {
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
         previewView = findViewById(R.id.cameraPreview)
         toggleFlash = findViewById(R.id.toggleFlash)
+        arrowImageView = findViewById(R.id.arrowImageView)
 
         val arrowImageView: ImageView = findViewById(R.id.arrowImageView)
         // Show arrow initially
@@ -78,11 +91,55 @@ class CameraView : AppCompatActivity() {
             toggleFlashIcon()
         }
 
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-//        requestLocationAndProcessDirections()
+        requestLocationAndProcessDirections()
+    }
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+
+        if (event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, 3)
+        } else if (event.sensor == magnetometer) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, 3)
+        }
+
+        val rotationMatrix = FloatArray(9)
+        val success = SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+
+        if (success) {
+            val orientationValues = FloatArray(3)
+            SensorManager.getOrientation(rotationMatrix, orientationValues)
+
+            // Calculate the angle based on the device's orientation
+            val angle = calculateAngle(orientationValues)
+            arrowImageView.rotation = angle
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Handle accuracy changes if needed
+    }
     private fun startCamera(cameraFacing: Int) {
         val aspectRatio = aspectRatio(previewView.width, previewView.height)
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -139,81 +196,87 @@ class CameraView : AppCompatActivity() {
         }
     }
 
-//    private fun requestLocationAndProcessDirections() {
-//        val locationRequest = LocationRequest.create().apply {
-//            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//            interval = 10000 // Update interval in milliseconds
-//        }
-//
-//        val locationCallback = object : LocationCallback() {
-//            override fun onLocationResult(locationResult: LocationResult?) {
-//                locationResult?.lastLocation?.let { location ->
-//                    val currentLatLng = LatLng(location.latitude, location.longitude)
-//
-//                    // Process the current location here
-//                    GlobalScope.launch(Dispatchers.IO) {
-//                        val directionsResult =
-//                            DirectionsUtils.getDirections(this@CameraView, currentLatLng, destination)
-//                        processDirectionsResult(directionsResult)
-//                    }
-//
-//                    // Remove the location updates since we only need the current location once
-//                    fusedLocationClient.removeLocationUpdates(this)
-//                }
-//            }
-//        }
-//
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//            == PackageManager.PERMISSION_GRANTED
-//        ) {
-//            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
-//        } else {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-//                LOCATION_PERMISSION_REQUEST_CODE
-//            )
-//        }
-//    }
-//
-//    private fun processDirectionsResult(directionsResult: DirectionsResult?) {
-//        if (directionsResult != null) {
-//            // Process the directions result, e.g., extract route information, duration, etc.
-//            val route = directionsResult.routes[0]
-//            val legs: List<DirectionsLeg> = route.legs.toList()
-//
-//            for (leg in legs) {
-//                val steps: List<DirectionsStep> = leg.steps.toList()
-//                val distance = leg.distance.humanReadable
-//                val duration = leg.duration.humanReadable
-//                val startAddress = leg.startAddress
-//                val endAddress = leg.endAddress
-//
-//                // Log directions details
-//                println("Distance: $distance")
-//                println("Duration: $duration")
-//                println("Start Address: $startAddress")
-//                println("End Address: $endAddress")
-//
-//                for (step in steps) {
-//                    val instruction = step.htmlInstructions
-//                    val stepDistance = step.distance.humanReadable
-//                    val stepDuration = step.duration.humanReadable
-//                    val startLocation = step.startLocation
-//                    val endLocation = step.endLocation
-//
-//                    // Log turn-by-turn instruction
-//                    println("Instruction: $instruction")
-//                    println("Distance: $stepDistance")
-//                    println("Duration: $stepDuration")
-//                    println("Start Location: ${startLocation.lat}, ${startLocation.lng}")
-//                    println("End Location: ${endLocation.lat}, ${endLocation.lng}")
-//                }
-//            }
-//        }
-//    }
-//
-//    companion object {
-//        private const val LOCATION_PERMISSION_REQUEST_CODE = 123
-//    }
+    private fun requestLocationAndProcessDirections() {
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10000 // Update interval in milliseconds
+        }
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult?.lastLocation?.let { location ->
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+
+                    // Process the current location here
+                    GlobalScope.launch(Dispatchers.IO) {
+                        val directionsResult =
+                            DirectionsUtils.getDirections(this@CameraView, currentLatLng, destination)
+                        processDirectionsResult(directionsResult)
+                    }
+
+                    // Remove the location updates since we only need the current location once
+                    fusedLocationClient.removeLocationUpdates(this)
+                }
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun processDirectionsResult(directionsResult: DirectionsResult?) {
+        if (directionsResult != null) {
+            // Process the directions result, e.g., extract route information, duration, etc.
+            val route = directionsResult.routes[0]
+            val legs: List<DirectionsLeg> = route.legs.toList()
+
+            for (leg in legs) {
+                val steps: List<DirectionsStep> = leg.steps.toList()
+                val distance = leg.distance.humanReadable
+                val duration = leg.duration.humanReadable
+                val startAddress = leg.startAddress
+                val endAddress = leg.endAddress
+
+                // Log directions details
+                println("Distance: $distance")
+                println("Duration: $duration")
+                println("Start Address: $startAddress")
+                println("End Address: $endAddress")
+
+                for (step in steps) {
+                    val instruction = step.htmlInstructions
+                    val stepDistance = step.distance.humanReadable
+                    val stepDuration = step.duration.humanReadable
+                    val startLocation = step.startLocation
+                    val endLocation = step.endLocation
+
+                    // Log turn-by-turn instruction
+                    println("Instruction: $instruction")
+                    println("Distance: $stepDistance")
+                    println("Duration: $stepDuration")
+                    println("Start Location: ${startLocation.lat}, ${startLocation.lng}")
+                    println("End Location: ${endLocation.lat}, ${endLocation.lng}")
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 123
+    }
+    private fun calculateAngle(orientationValues: FloatArray): Float {
+        // Calculate the angle based on the device's orientation
+        // You can adjust the formula as needed to control the arrow's behavior
+        val azimuth = Math.toDegrees(orientationValues[0].toDouble()).toFloat()
+        return -azimuth // Invert the angle for correct rotation
+    }
 }
